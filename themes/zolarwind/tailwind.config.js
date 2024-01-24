cb80f6e159c7c68074b61efeafc5ea6605b86c97
dxps <marius.ileana@gmail.com>
2024-01-24 12:53:31 +0200

    feat: blog content, grid gap space

--- themes/zolarwind/tailwind.config.js
+++ themes/zolarwind/tailwind.config.js
@@ -5,6 +5,10 @@ const { colors: defaultColors } = require('tailwindcss/defaultTheme')
 module.exports = {
     content: ['./themes/**/*.html', './templates/**/*.html', './content/**/*.md'],
     theme: {
+        gap: {
+            '10': '2.5rem', /* 40px */
+            '11': '2.75rem',
+        },
         extend: {
             colors: {
                 ...defaultColors,
@@ -12,7 +16,7 @@ module.exports = {
                     green0: '#86efac',
                     green1: '#4ade80',
                 },
-            }
+            },
         },
     },
     variants: {
